3f642bf566cc31281e58659581da570f7cc7083a
fernandotorres25 <fer.torres08@hotmail.com>
2022-10-09 04:35:50 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/mycompany/clases/Choferes.java
@@ -0,0 +1,9 @@
+package com.mycompany.clases;
+
+public class Choferes {
+    
+    private int DNI;
+    private int gastos_totales;
+    private int cant_viajes;
+    
+}
---
+++ src/main/java/com/mycompany/clases/Vehiculos.java
@@ -0,0 +1,8 @@
+package com.mycompany.clases;
+
+public class Vehiculos {
+    
+    private String patente;
+    private int gastos_totales;
+    private int cant_viajes;
+}
